7e38f167eacb41b49d59213307de4c111cb490e1
Luha Yang <124209885+luhayang@users.noreply.github.com>
2024-02-02 12:13:48 -0600

    Update testquery.py

--- testquery.py
+++ testquery.py
@@ -19,7 +19,8 @@ def test_connection():
     conn.close()
     return None
 
-def test_query_one():
+# This function prints top 5 counties voted the most for Trump in 2016
+def execute_query_one():
 
     conn = psycopg2.connect(
         host="localhost",
@@ -30,23 +31,26 @@ def test_query_one():
 
     cur = conn.cursor()
 
-    sql = """SELECT county, state, trump16 FROM elections WHERE trump16 IS NOT NULL AND trump16 > 0.75 ORDER BY trump16 DESC;"""
+    sql = """SELECT county, state, trump16 FROM elections WHERE trump16 IS NOT NULL AND trump16 > 0.75 ORDER BY trump16 DESC LIMIT 5;"""
 
     cur.execute( sql )
 
-    row = cur.fetchone()
+    row_list = cur.fetchall()
 
     if row == None:
         print("No counties voted for Trump in 2016 by more than 75%. \n")
     else:
-        print("In 2016, the county {}, {} voted the most for Trump by {:.2f}%. \n".format(row[0], row[1], round(row[2], 4)*100))
+        print("In 2016, top 5 counties that voted the most for Trump are:)
+        for row in row_list:
+            print("The county {}, {} voted for Trump by {:.2f}%. \n".format(row[0], row[1], round(row[2], 4)*100))
     
     conn.commit()
     cur.close()
     conn.close()
     return None
 
-def test_query_two():
+# This function determines which county had the highest population, then prints which percentage of votes towards Trump in 2020
+def execute_query_two():
 
     conn = psycopg2.connect(
         host="localhost",
@@ -73,7 +77,8 @@ def test_query_two():
     conn.close()
     return None
     
-def test_query_three():
+# This function determines which county had the highest total vote, then prints which percentage of votes towards Trump and Biden in 2020
+def execute_query_three():
 
     conn = psycopg2.connect(
         host="localhost",
@@ -100,7 +105,8 @@ def test_query_three():
     conn.close()
     return None    
 
-def test_query_four():
+# This function determines which county had the highest Black population, then prints which percentage of votes towards Trump and Clinton in 2016
+def execute_query_four():
 
     conn = psycopg2.connect(
         host="localhost",
@@ -126,8 +132,9 @@ def test_query_four():
     cur.close()
     conn.close()
     return None
-    
-def test_query_five():
+
+# This function prints which county in NY had the lowest votes towards Trump in 2020
+def execute_query_five():
 
     conn = psycopg2.connect(
         host="localhost",
@@ -154,9 +161,12 @@ def test_query_five():
     conn.close()
     return None
 
+
 test_connection()
-test_query_one()
-test_query_two()
-test_query_three()
-test_query_four()
-test_query_five()
+
+# Functions below send an SQL query to the database
+execute_query_one()
+execute_query_two()
+execute_query_three()
+execute_query_four()
+execute_query_five()
